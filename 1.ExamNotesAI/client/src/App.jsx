2c358163a18801aec7c16ffe9c00e350b5649fc1
varunsaini28 <vs1536375@gmail.com>
2026-02-20 21:22:06 +0530

    Update App.jsx

--- 1.ExamNotesAI/client/src/App.jsx
+++ 1.ExamNotesAI/client/src/App.jsx
@@ -9,7 +9,7 @@ import Notes from './pages/Notes'
 import Pricing from './pages/Pricing'
 import PaymentSuccess from './pages/PaymentSuccess'
 import PaymentFailed from './pages/PaymentFailed'
-export const serverUrl = "https://exam-notes-ai2client.onrender.com"
+export const serverUrl = "https://exam-notes-ai2server.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
